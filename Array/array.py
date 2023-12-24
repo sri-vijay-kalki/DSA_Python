58e512f42f90f04cef4a5fe6fdf0ce7723cfcd74
poosarla-sri-vijay-kalki <srivijaykalki@gmail.com>
2023-12-23 19:53:51 -0500

    [ADD]: ADD Array content

---
+++ Array/array.py
@@ -0,0 +1,14 @@
+import array
+
+my_array= array.array('i')
+print(my_array)
+
+my_array1 = array.array("i", [1,2,3,4,5])
+print(my_array1)
+
+import numpy as np
+np_array= np.array([],dtype=int)
+print(np_array)
+
+np_array1= np.array([1.1j,2,3,4,5])
+print(np_array1)
